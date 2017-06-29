095194bb616cb41ab5075108158ea06cf10e95a9
root <fernando.bac03@hotmail.com>
2017-06-29 16:53:12 +0200

    moving divs

--- ckanext/repeating/theme/public/scripts/group.js
+++ ckanext/repeating/theme/public/scripts/group.js
@@ -6,19 +6,14 @@ function add_field_in_group(index, label){
 	document.getElementById(nextelementid).style.display = 'block';
 	var buttonid='button_fields_repeating_container_'+label+'_'+index;
 	document.getElementById(buttonid).style.display = 'none';
-	MoverTexto(elementid, nextelementid, index);
+	MoverDiv(elementid, nextelementid, index);
 }
 
 
-function MoverTexto(elementid, nextelementid, index){
-
-	var newDiv = document.createElement("div");
-
-	
-        var otro = document.getElementById('fields_repeating_container_Emailss_'+index;);
+function MoverDiv(elementid, nextelementid, index){
+	var newDiv = document.createElement("div");	
+        var otro = document.getElementById('fields_repeating_container_Emailss_'+index);
 	var fieldinput = document.getElementById(elementid);
-
 	fieldinput.appendChild(otro);
-
 }
 
