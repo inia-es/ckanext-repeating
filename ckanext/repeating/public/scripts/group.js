8d654ba63432c13db1b1b0e01368b71a20bc5538
root <fernando.bac03@hotmail.com>
2017-06-29 13:16:12 +0200

    deleting old files

--- ckanext/repeating/public/scripts/group.js
+++
@@ -1,9 +0,0 @@
-
-function add_field_in_group(index, label){
-	var next=index+1;
-	var elementid='fields_repeating_container_'+label+'_'+next;
-	document.getElementById(elementid).style.display = 'block';
-	var buttonid='button_fields_repeating_container_'+label+'_'+index;
-	document.getElementById(buttonid).style.display = 'none';
-}
-
