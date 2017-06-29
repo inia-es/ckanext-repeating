5df091ebb8089605ab39b6775c349f7060150eb7
root <fernando.bac03@hotmail.com>
2017-06-29 14:40:08 +0200

    setting positions for new fields

--- ckanext/repeating/theme/public/scripts/group.js
+++ ckanext/repeating/theme/public/scripts/group.js
@@ -1,9 +1,34 @@
 
 function add_field_in_group(index, label){
 	var next=index+1;
-	var elementid='fields_repeating_container_'+label+'_'+next;
-	document.getElementById(elementid).style.display = 'block';
+	var elementid = 'fields_repeating_container_'+label+'_'+index;
+	var nextelementid='fields_repeating_container_'+label+'_'+next;
+	document.getElementById(nextelementid).style.display = 'block';
 	var buttonid='button_fields_repeating_container_'+label+'_'+index;
 	document.getElementById(buttonid).style.display = 'none';
+	MoverTexto(elementid, nextelementid, "A");
+}
+
+
+function MoverTexto(elementid, nextelementid,d){
+
+ var fieldinput=document.getElementById(elementid);
+ var nextfieldinput=document.getElementById(nextelementid);
+
+ var y=(document.defaultView && document.defaultView.getComputedStyle) ?
+        document.defaultView.getComputedStyle(fieldinput,'').getPropertyValue("top") :
+        fieldinput.currentStyle ? fieldinput.currentStyle.top : "";
+ var x=(document.defaultView && document.defaultView.getComputedStyle) ?
+        document.defaultView.getComputedStyle(fieldinput,'').getPropertyValue("left") :
+        fieldinput.currentStyle ? fieldinput.currentStyle.left : "";
+
+ x= parseInt(x);
+ y= parseInt(y);
+
+switch(d){
+ case "A":  y +=100; break;
+}
+nextfieldinput.style.top=y+"px";
+nextfieldinput.style.left=nextfieldinput.style.left;
 }
 
