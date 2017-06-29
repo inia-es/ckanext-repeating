26e89c2adb2ba83d5d89f6f6a96f7170ce0ed6e1
root <fernando.bac03@hotmail.com>
2017-06-29 16:46:17 +0200

    moving divs

--- ckanext/repeating/theme/public/scripts/group.js
+++ ckanext/repeating/theme/public/scripts/group.js
@@ -6,33 +6,19 @@ function add_field_in_group(index, label){
 	document.getElementById(nextelementid).style.display = 'block';
 	var buttonid='button_fields_repeating_container_'+label+'_'+index;
 	document.getElementById(buttonid).style.display = 'none';
-	MoverTexto(elementid, nextelementid, "A");
+	MoverTexto(elementid, nextelementid, index);
 }
 
 
-function MoverTexto(elementid, nextelementid,d){
+function MoverTexto(elementid, nextelementid, index){
 
- var fieldinput=document.getElementById(elementid);
- var nextfieldinput=document.getElementById(nextelementid);
+	var newDiv = document.createElement("div");
 
-var posicion = fieldinput.position();
-var y = posicion.top;
-var x = posicion.left;
-/**
- var y=(document.defaultView && document.defaultView.getComputedStyle) ?
-        document.defaultView.getComputedStyle(fieldinput,'').getPropertyValue("top") :
-        fieldinput.currentStyle ? fieldinput.currentStyle.top : "";
- var x=(document.defaultView && document.defaultView.getComputedStyle) ?
-        document.defaultView.getComputedStyle(fieldinput,'').getPropertyValue("left") :
-        fieldinput.currentStyle ? fieldinput.currentStyle.left : "";
-**/
- x= parseInt(x);
- y= parseInt(y);
+	
+        var otro = document.getElementById('fields_repeating_container_Emailss_'+index;);
+	var fieldinput = document.getElementById(elementid);
 
-switch(d){
- case "A":  y +=100; break;
-}
-nextfieldinput.style.top="100px";
-nextfieldinput.style.left=nextfieldinput.style.left;
+	fieldinput.appendChild(otro);
+
 }
 
